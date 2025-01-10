00b7ce3836cbe65cc2c71d622bbff01cb7e81169
Balcus <bbalcus04@gmail.com>
2025-01-10 21:16:43 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void parse(char* command, char** args, int* argc) {
+
+}
+
+int system(char * const command){
+   /* your code goes here */
+}
+
+int main(int argc, char *argv[], char *env[]) {
+
+    return 0;
+}
